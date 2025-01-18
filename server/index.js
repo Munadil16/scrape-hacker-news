deae52b57f38ceb90f6622e75a7c407ce077bba1
Munadil16 <munadilxd@gmail.com>
2025-01-18 18:14:58 +0530

    feat: Add websockets and logic for initial connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,47 @@
 import "dotenv/config";
+import axios from "axios";
 import express from "express";
+import * as cheerio from "cheerio";
+import { WebSocketServer } from "ws";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`)
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
+
+const wss = new WebSocketServer({ server });
+
+const onInitialConnection = async () => {
+    try {
+        const res = await axios.get("https://news.ycombinator.com/newest");
+        const $ = cheerio.load(res.data);
+        let count = 0;
+
+        $('.age').each((_, element) => {
+            const duration = Number($(element).text().split(" ")[0]);
+            if (duration > 5) {
+                return false;
+            }
+
+            count++;
+        });
+
+        return count;
+    } catch (err) {
+        console.log("Error while getting data during initial connection: ", err.message)
+    }
+}
+
+wss.on("connection", async (ws) => {
+    ws.on("error", (err) => console.error(err));
+
+    try {
+        const msg = await onInitialConnection();
+        ws.send(`Number of stories in last 5 minutes: ${msg.toString()}`);
+    } catch (err) {
+        console.log("Error while sending message: ", err);
+    }
 });
 
